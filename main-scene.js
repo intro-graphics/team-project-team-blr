a7478cd598863935e73af523a40e6c0be4cf53fb
Ricky <horicky55@yahoo.com>
2020-05-17 20:37:11 -0700

    Static Environment Implementation
    Creates the static environment including the court, walls, and ball. Changes README file to our project's description

--- main-scene.js
+++ main-scene.js
@@ -5,15 +5,15 @@ class Basketball_Scene extends Scene_Component
         if( !context.globals.has_controls   ) 
           context.register_scene_component( new Movement_Controls( context, control_box.parentElement.insertCell() ) ); 
 
-        context.globals.graphics_state.camera_transform = Mat4.look_at( Vec.of( 0,9,25 ), Vec.of( 0,9,0 ), Vec.of( 0,1,0 ) );//Vec.of( 0,10,20 ), Vec.of( 0,0,0 ), Vec.of( 0,1,0 ) );
+        context.globals.graphics_state.camera_transform = Mat4.look_at( Vec.of( 17,10,0 ), Vec.of( 0,8,0 ), Vec.of( 0,1,0 ) );//Vec.of( 0,10,20 ), Vec.of( 0,0,0 ), Vec.of( 0,1,0 ) );
         this.initial_camera_location = Mat4.inverse( context.globals.graphics_state.camera_transform );
 
         const r = context.width/context.height;
         context.globals.graphics_state.projection_transform = Mat4.perspective( Math.PI/4, r, .1, 1000 );
 
-        const shapes = { 
+        const shapes = { square: new Square(),
                          sphere4: new Subdivision_Sphere( 4 ),
-                         board: new Cube(),
+                         cube: new Cube(),
                          text: new Text_Line(10)
                        }
 
@@ -21,29 +21,44 @@ class Basketball_Scene extends Scene_Component
                                      
                                      // Make some Material objects available to you:
         this.materials =
-          { test:     context.get_instance( Phong_Shader ).material( Color.of( 1,1,0,1 ), { ambient:.2 } ),
+          { test:     context.get_instance( Phong_Shader ).material( Color.of( 1, 1, 0, 1 ), { ambient:.2 } ),
 
-            ball:     context.get_instance( Phong_Shader ).material( Color.of( 1,125/255, 0, 1 ), {
+            ball:     context.get_instance( Phong_Shader ).material( Color.of( 0, 0, 0, 1 ), {
+                        ambient: 1,
+                        diffusivity: 0,
+                        specularity: 0,
+                        texture: context.get_instance("assets/ball.png", false) } ),
+
+            board:    context.get_instance( Phong_Shader ).material( Color.of( 0.15, 0.15, 0.15, 1 ), {
                         ambient: 1,
                         diffusivity: 0,
                         specularity: 0  } ),
 
-            board:    context.get_instance( Phong_Shader ).material( Color.of( 1,1,1,1 ), {
-                        ambient: 1,
-                        diffusivity: 0,
-                        specularity: 0  } ),
-
-            text_img: context.get_instance( Phong_Shader ).material( Color.of( 1,1,1,1 ), {
+            text_img: context.get_instance( Phong_Shader ).material( Color.of( 1, 1, 1, 1 ), {
                         ambient: 1,
                         diffusivity: 0,
                         specularity: 0,
                         texture: context.get_instance("assets/text.png", false) } ),
+
+            ground:   context.get_instance( Phong_Shader ).material( Color.of( 0, 0, 0, 1), {
+                        ambient: 1,
+                        diffusivity: 0,
+                        specularity: 0,
+                        texture: context.get_instance("assets/court.png", false) } ),
+
+            wall:     context.get_instance( Phong_Shader ).material( Color.of( 0, 0, 0, 1 ), {
+                        ambient: 1,
+                        diffusivity: 0,
+                        specularity: 0,
+                        texture: context.get_instance("assets/walls.png", false),
+            }),
     
           }
 
         this.lights = [ new Light( Vec.of( 5,-10,5,1 ), Color.of( 0, 1, 1, 1 ), 1000 ) ];
 
-        this.score = 0
+        // Attributes for Basketball_Scene
+        this.score = 0;
 
       }
 
@@ -62,12 +77,40 @@ class Basketball_Scene extends Scene_Component
       { graphics_state.lights = this.lights;        // Use the lights stored in this.lights.
         const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
 
-        this.shapes.sphere4.draw( graphics_state, Mat4.identity(), this.materials.ball );
+        // Draw the static basketball - NEEDS MODIFICATION FOR MOUSE PICKING
+        let ball_transform = Mat4.identity();
+        ball_transform = ball_transform.times(Mat4.rotation( Math.PI/2, Vec.of(0,1,0) ));
+        ball_transform = ball_transform.times(Mat4.translation( [0,1,-5] ));
+        this.shapes.sphere4.draw( graphics_state, ball_transform, this.materials.ball );
 
+        // Draw the scoreboard 
         let scoreboard_transform = Mat4.identity();
-        scoreboard_transform = scoreboard_transform.times(Mat4.translation( [-15.6,17.3,0] ));
-        scoreboard_transform = scoreboard_transform.times(Mat4.scale( [3,2,.01] ));
-        this.shapes.board.draw( graphics_state, scoreboard_transform, this.materials.board)
+        scoreboard_transform = scoreboard_transform.times(Mat4.rotation( Math.PI/2, Vec.of(0,1,0)));
+        scoreboard_transform = scoreboard_transform.times(Mat4.translation( [-15,19,-35] ));
+        scoreboard_transform = scoreboard_transform.times(Mat4.scale( [8,4,.25] ));
+        this.shapes.cube.draw( graphics_state, scoreboard_transform, this.materials.board)
+
+        // Draw the ground 
+        let ground_transform = Mat4.identity();
+        ground_transform = ground_transform.times(Mat4.rotation( Math.PI/2, Vec.of(1,0,0) ));
+        ground_transform = ground_transform.times(Mat4.scale( [35,25,1] ));
+        this.shapes.square.draw( graphics_state, ground_transform, this.materials.ground);
+
+        // Draw the walls 
+        let wall_transform = Mat4.identity();
+        wall_transform = wall_transform.times(Mat4.translation( [0,15,25] ));
+        wall_transform = wall_transform.times(Mat4.scale( [35,15,0] ));
+        this.shapes.square.draw( graphics_state, wall_transform, this.materials.wall);  // Left wall
+        wall_transform = Mat4.identity();
+        wall_transform = wall_transform.times(Mat4.translation( [0,15,-25] ));
+        wall_transform = wall_transform.times(Mat4.scale( [35,15,0 ] ));
+        this.shapes.square.draw( graphics_state, wall_transform, this.materials.wall);  // Right wall
+        wall_transform = Mat4.identity();
+        wall_transform = wall_transform.times(Mat4.rotation( Math.PI/2, Vec.of(0,1,0)));
+        wall_transform = wall_transform.times(Mat4.translation( [0,15,-35] ));
+        wall_transform = wall_transform.times(Mat4.scale( [25,15,0]));
+        this.shapes.square.draw( graphics_state, wall_transform, this.materials.wall);  // Front wall
+
 
       }
   }
