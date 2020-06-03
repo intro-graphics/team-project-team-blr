8ebe7fe95d2b0a75c7fac774dda1254ba1782594
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2020-06-02 23:25:10 -0700

    New tinygraphics version

--- main-scene.js
+++ main-scene.js
@@ -1,145 +1,49 @@
-window.Basketball_Scene = window.classes.Basketball_Scene =
-class Basketball_Scene extends Scene_Component
-  { constructor( context, control_box )     // The scene begins by requesting the camera, shapes, and materials it will need.
-      { super(   context, control_box );    // First, include a secondary Scene that provides movement controls:
-        if( !context.globals.has_controls   ) 
-          context.register_scene_component( new Movement_Controls( context, control_box.parentElement.insertCell() ) ); 
+import {tiny, defs} from './examples/common.js';
+                                                  // Pull these names into this module's scope for convenience:
+const { Vector, Vector3, vec, vec3, vec4, color, Matrix, Mat4, Light, Shape, Material, Shader, Texture, Scene,
+        Canvas_Widget, Code_Widget, Text_Widget } = tiny;
 
-        context.globals.graphics_state.camera_transform = Mat4.look_at( Vec.of( 0,10,18 ), Vec.of( 0,5,-20 ), Vec.of( 0,1,0 ) );
-        this.initial_camera_location = Mat4.inverse( context.globals.graphics_state.camera_transform );
+    // Now we have loaded everything in the files tiny-graphics.js, tiny-graphics-widgets.js, and common.js.
+    // This yielded "tiny", an object wrapping the stuff in the first two files, and "defs" for wrapping all the rest.
 
-        const r = context.width/context.height;
-        context.globals.graphics_state.projection_transform = Mat4.perspective( Math.PI/4, r, .1, 1000 );
+    // ******************** Extra step only for when executing on a local machine:  
+    //                      Load any more files in your directory and copy them into "defs."
+    //                      (On the web, a server should instead just pack all these as well 
+    //                      as common.js into one file for you, such as "dependencies.js")
 
-        const shapes = { square: new Square(),
-                         sphere4: new Subdivision_Sphere( 4 ),
-                         cube: new Cube(),
-                         text: new Text_Line(10)
-                       }
+const Minimal_Webgl_Demo = defs.Minimal_Webgl_Demo;
+import { Axes_Viewer, Axes_Viewer_Test_Scene } 
+  from "./examples/axes-viewer.js"
+import { Inertia_Demo, Collision_Demo }
+  from "./examples/collisions-demo.js"
+import { Many_Lights_Demo }
+  from "./examples/many-lights-demo.js"
+import { Obj_File_Demo }
+  from "./examples/obj-file-demo.js"
+import { Scene_To_Texture_Demo }
+  from "./examples/scene-to-texture-demo.js"
+import { Surfaces_Demo }
+  from "./examples/surfaces-demo.js"
+import { Text_Demo }
+  from "./examples/text-demo.js"
+import { Transforms_Sandbox }
+  from "./examples/transforms-sandbox.js"
 
-        this.submit_shapes( context, shapes );
-                                     
-        // Make some Material objects available to you:
-        this.materials =
-          { test:     context.get_instance( Phong_Shader ).material( Color.of( 1, 1, 0, 1 ), { ambient:.2 } ),
+Object.assign( defs,
+                     { Axes_Viewer, Axes_Viewer_Test_Scene },
+                     { Inertia_Demo, Collision_Demo },
+                     { Many_Lights_Demo },
+                     { Obj_File_Demo },
+                     { Scene_To_Texture_Demo },
+                     { Surfaces_Demo },
+                     { Text_Demo },
+                     { Transforms_Sandbox } );
 
-            ball:     context.get_instance( Phong_Shader ).material( Color.of( 0, 0, 0, 1 ), {
-                        ambient: 1,
-                        diffusivity: 0,
-                        specularity: 0,
-                        texture: context.get_instance("assets/ball.png", true) } ),
+    // ******************** End extra step
 
-            board:    context.get_instance( Phong_Shader ).material( Color.of( 0.15, 0.15, 0.15, 1 ), {
-                        ambient: 1,
-                        diffusivity: 0,
-                        specularity: 0  } ),
+// (Can define Main_Scene's class here)
 
-            text_img: context.get_instance( Phong_Shader ).material( Color.of( 1, 1, 1, 1 ), {
-                        ambient: 1,
-                        diffusivity: 0,
-                        specularity: 0,
-                        texture: context.get_instance("assets/text.png", false) } ),
+const Main_Scene = Obj_File_Demo;
+const Additional_Scenes = [];
 
-            ground:   context.get_instance( Phong_Shader ).material( Color.of( 0, 0, 0, 1), {
-                        ambient: 1,
-                        diffusivity: 0,
-                        specularity: 0,
-                        texture: context.get_instance("assets/court.png", true) } ),
-
-            wall:     context.get_instance( Phong_Shader ).material( Color.of( 0, 0, 0, 1 ), {
-                        ambient: 0.9,
-                        diffusivity: 0,
-                        specularity: 0,
-                        texture: context.get_instance("assets/walls.png", true),
-            }),
-          }
-
-        this.lights = [ new Light( Vec.of( 5,-10,5,1 ), Color.of( 0, 1, 1, 1 ), 1000 ) ];
-
-        /* ================================= ATTRIBUTES FOR BASKETBALL_SCENE ========================================= */
-   
-        this.mouseX = 0;
-        this.mouseY = 0;
-        context.canvas.addEventListener("mousemove", this.mouse_tracker.bind(this));
-        context.canvas.addEventListener("mousedown", this.cast_ray.bind(this));
-
-        this.ball_transform = Mat4.identity().times(Mat4.translation( [0,1,-5] ));
-
-        /* =========================================================================================================== */
-      }
-
-    mouse_tracker(event)        // Mouse tracker for our canvas
-      {
-        var rect = document.getElementById("main-canvas").getBoundingClientRect();
-        this.mouseX = event.clientX - (rect.left + rect.right)/2;
-        this.mouseY = -1 * (event.clientY - (rect.bottom + rect.top - 379)/2);
-        //console.log(this.mouseX, this.mouseY);
-      }
-
-    intersectSphere()         // Check if the ray intersects a sphere 
-     { 
-
-     }
-
-    cast_ray()       
-      {
-        if (this.mouseX >= -540 && this.mouseX <= 540 && this.mouseY >= -300 && this.mouseY <= 300)     // Only perform ray cast if we clicked on the scene
-          {  
-            console.log(this.mouseX, this.mouseY); 
-//             let norm_x = this.mouseX / 540.;    // Convert to WebGL coordinates x,y:[-1,1]
-//             let norm_y = this.mouseY / 300.;
-
-//             let ray_proj = Vec.of(norm_x, norm_y, -1, 1);
-//             let ray_eye = Mat4.inverse(Mat4.perspective( Math.PI/4, 1080/600, .1, 1000 )) * ray_proj;
-//             console.log(ray_eye);
-          }
-        
-      }
-
-    make_control_panel()            // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
-      { this.key_triggered_button( "View scene",  [ "0" ], () => this.attached = () => this.initial_camera_location );
-        this.new_line();
-
-      }
-
-    display( graphics_state )
-      { graphics_state.lights = this.lights;        // Use the lights stored in this.lights.
-        const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
-
-        // Draw the basketball - NEEDS MODIFICATION FOR MOUSE PICKING
-        this.shapes.sphere4.draw( graphics_state, this.ball_transform, this.materials.ball );
-
-        // Draw the scoreboard
-        let scoreboard_transform = Mat4.identity();
-        scoreboard_transform = scoreboard_transform.times(Mat4.translation( [-17,20,-35] ));
-        scoreboard_transform = scoreboard_transform.times(Mat4.scale( [7,4,.25] ));
-        this.shapes.cube.draw( graphics_state, scoreboard_transform, this.materials.board);
-
-        // Draw the ground 
-        let ground_transform = Mat4.identity();
-        ground_transform = ground_transform.times(Mat4.rotation( Math.PI/2, Vec.of(0,1,0) ));
-        ground_transform = ground_transform.times(Mat4.rotation( Math.PI/2, Vec.of(1,0,0) ));
-        ground_transform = ground_transform.times(Mat4.scale( [35,25,1] ));
-        this.shapes.square.draw( graphics_state, ground_transform, this.materials.ground);
-
-        // Draw the walls 
-        let wall_transform = Mat4.identity();
-        wall_transform = wall_transform.times(Mat4.rotation( Math.PI/2, Vec.of(0,1,0) ));
-        wall_transform = wall_transform.times(Mat4.translation( [0,15,25] ));
-        wall_transform = wall_transform.times(Mat4.scale( [35,15,0] ));
-        this.shapes.square.draw( graphics_state, wall_transform, this.materials.wall);  // Left wall
-        wall_transform = Mat4.identity();
-        wall_transform = wall_transform.times(Mat4.rotation( Math.PI/2, Vec.of(0,1,0) ));
-        wall_transform = wall_transform.times(Mat4.translation( [0,15,-25] ));
-        wall_transform = wall_transform.times(Mat4.scale( [35,15,0 ] ));
-        this.shapes.square.draw( graphics_state, wall_transform, this.materials.wall);  // Right wall
-        wall_transform = Mat4.identity();
-        wall_transform = wall_transform.times(Mat4.rotation( -Math.PI/2, Vec.of(0,1,0) ));
-        wall_transform = wall_transform.times(Mat4.rotation( Math.PI/2, Vec.of(0,1,0)));
-        wall_transform = wall_transform.times(Mat4.translation( [0,15,-35] ));
-        wall_transform = wall_transform.times(Mat4.scale( [25,15,0]));
-        this.shapes.square.draw( graphics_state, wall_transform, this.materials.wall);  // Front wall
-
-
-      }
-  }
+export { Main_Scene, Additional_Scenes, Canvas_Widget, Code_Widget, Text_Widget, defs }
